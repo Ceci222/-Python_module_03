82bac478b03c6567db8de29bf9b35390115bc5a2
ceci222 <ceci_2504@hotmail.com>
2026-05-27 03:11:25 +0200

    finish parser

--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -1,11 +1,21 @@
 import sys
 
 
-def arg_parser(args: list[str]) -> dict[str,int]:
-    parsed_data = dict[str,int] = {}
-    for arg in args[1:]:
+def arg_parser(args: list[str]) -> dict[str, int]:
+    parsed_data: dict[str, int] = {}
+    for arg in args:
         parts = arg.split(":")
         if len(parts) != 2:
-            print(f"Error - invalid parameter {arg}")
+            print(f"Error - invalid parameter '{arg}'")
             continue
+        name, quantity_str = parts
+        try:
+            quantity = int(quantity_str)  #if it's not "3" like "a" = error
+        except ValueError as e:
+            print(f"Quantity error for '{name}': {e}")
+            continue
+        if name in parsed_data:
+            print(f"Redundant item '{name}' - discarding")
+            continue
+        parsed_data[name] = quantity  # I add quantity only once it's been checked
     return parsed_data
